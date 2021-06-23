b234bb136127d2786b35834b6d092014095ef221
Rolando Febrianto <73069701+ndoworry@users.noreply.github.com>
2021-06-23 10:44:14 +0700

    Add files via upload

---
+++ Kasirapp/Kasirapp/MainForm.Designer.cs
@@ -0,0 +1,284 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: MSI Alpha
+ * Date: 24/05/2021
+ * Time: 0:21
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace Kasirapp
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.textBox5 = new System.Windows.Forms.TextBox();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.label7 = new System.Windows.Forms.Label();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.textBox7 = new System.Windows.Forms.TextBox();
+			this.button4 = new System.Windows.Forms.Button();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			this.groupBox1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(118, 94);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(343, 20);
+			this.textBox1.TabIndex = 0;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(531, 135);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "Simpan";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(12, 91);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(100, 23);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "Kode Barang";
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Location = new System.Drawing.Point(12, 359);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.Size = new System.Drawing.Size(622, 271);
+			this.dataGridView1.TabIndex = 3;
+			this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DataGridView1CellClick);
+			// 
+			// label2
+			// 
+			this.label2.Location = new System.Drawing.Point(12, 117);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(100, 23);
+			this.label2.TabIndex = 4;
+			this.label2.Text = "Nama Barang";
+			// 
+			// label3
+			// 
+			this.label3.Location = new System.Drawing.Point(12, 143);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(100, 23);
+			this.label3.TabIndex = 5;
+			this.label3.Text = "Harga Jual";
+			// 
+			// label4
+			// 
+			this.label4.Location = new System.Drawing.Point(12, 169);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(100, 23);
+			this.label4.TabIndex = 6;
+			this.label4.Text = "Harga Beli";
+			// 
+			// label5
+			// 
+			this.label5.Location = new System.Drawing.Point(12, 195);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(100, 23);
+			this.label5.TabIndex = 7;
+			this.label5.Text = "Jumlah Barang";
+			// 
+			// label6
+			// 
+			this.label6.Location = new System.Drawing.Point(12, 221);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(100, 23);
+			this.label6.TabIndex = 8;
+			this.label6.Text = "Satuan";
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(118, 120);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(343, 20);
+			this.textBox2.TabIndex = 9;
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(118, 146);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(343, 20);
+			this.textBox3.TabIndex = 10;
+			// 
+			// textBox4
+			// 
+			this.textBox4.Location = new System.Drawing.Point(118, 172);
+			this.textBox4.Name = "textBox4";
+			this.textBox4.Size = new System.Drawing.Size(343, 20);
+			this.textBox4.TabIndex = 11;
+			// 
+			// textBox5
+			// 
+			this.textBox5.Location = new System.Drawing.Point(118, 198);
+			this.textBox5.Name = "textBox5";
+			this.textBox5.Size = new System.Drawing.Size(343, 20);
+			this.textBox5.TabIndex = 12;
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(531, 164);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 14;
+			this.button2.Text = "Update";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.Button2Click);
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(531, 193);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(75, 23);
+			this.button3.TabIndex = 15;
+			this.button3.Text = "Delete";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.Button3Click);
+			// 
+			// label7
+			// 
+			this.label7.Font = new System.Drawing.Font("Montserrat", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label7.Location = new System.Drawing.Point(211, 29);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(232, 31);
+			this.label7.TabIndex = 16;
+			this.label7.Text = "Kasir Rolando Cellular";
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.textBox7);
+			this.groupBox1.Location = new System.Drawing.Point(118, 265);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(343, 52);
+			this.groupBox1.TabIndex = 17;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "Pencarian Kode Barang";
+			// 
+			// textBox7
+			// 
+			this.textBox7.Location = new System.Drawing.Point(6, 19);
+			this.textBox7.Name = "textBox7";
+			this.textBox7.Size = new System.Drawing.Size(331, 20);
+			this.textBox7.TabIndex = 0;
+			this.textBox7.TextChanged += new System.EventHandler(this.TextBox7TextChanged);
+			// 
+			// button4
+			// 
+			this.button4.Location = new System.Drawing.Point(531, 224);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(75, 23);
+			this.button4.TabIndex = 18;
+			this.button4.Text = "Refresh";
+			this.button4.UseVisualStyleBackColor = true;
+			this.button4.Click += new System.EventHandler(this.Button4Click);
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Location = new System.Drawing.Point(118, 224);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(141, 21);
+			this.comboBox1.TabIndex = 19;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(644, 653);
+			this.Controls.Add(this.comboBox1);
+			this.Controls.Add(this.button4);
+			this.Controls.Add(this.groupBox1);
+			this.Controls.Add(this.label7);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.textBox5);
+			this.Controls.Add(this.textBox4);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.dataGridView1);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.textBox1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.Name = "MainForm";
+			this.Text = "Kasirapp";
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+		}
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.TextBox textBox7;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.TextBox textBox5;
+		private System.Windows.Forms.TextBox textBox4;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.DataGridView dataGridView1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox1;
+	}
+}
---
+++ Kasirapp/Kasirapp/MainForm.cs
@@ -0,0 +1,239 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: MSI Alpha
+ * Date: 24/05/2021
+ * Time: 0:21
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Kasirapp
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+
+		SqlConnection co = new SqlConnection("Server = MSI; Database = Dbkasir; integrated security = true");
+		SqlCommand mycommand = new SqlCommand();
+		SqlDataAdapter myadapter = new SqlDataAdapter();
+		
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			Bersihkan();
+			readdata();
+			MunculSatuan();
+			
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		
+		void Bersihkan() {
+			textBox1.Text="";
+			textBox2.Text="";
+			textBox3.Text="0";
+			textBox4.Text="0";
+			textBox5.Text="0";
+			comboBox1.Text="";
+			textBox7.Text="";
+			readdata();
+		}
+		
+		
+		void MunculSatuan() {
+			comboBox1.Items.Add("Unit");
+			comboBox1.Items.Add("Pcs");
+			comboBox1.Items.Add("Slot");
+
+		}
+	
+		void Nootomatis() {
+			long hitung;
+			string urutan;
+			SqlDataReader rd;
+			mycommand.Connection = co;
+                myadapter.SelectCommand = mycommand;
+                mycommand.CommandText = "select KodeBarang from Table_Barang where KodeBarang in(select max(kodeBarang) from Table_Barang) order by desc"; 
+                rd = mycommand.ExecuteReader();
+                rd.Read();
+                if (rd.HasRows) {
+                	hitung = Convert.ToInt64(rd[0].ToString().Substring(rd["KodeBarang"].ToString().Length - 3,3)) +1;
+                	string kodeurutan = "000" +hitung;
+                	urutan = "BRG" +kodeurutan.Substring(kodeurutan.Length - 3, 3);
+                }
+                else {
+                	urutan = "BRG001";
+                }
+                rd.Close();
+                textBox1.Text = urutan;
+                co.Close();
+		}
+		
+		void readdata() {
+			
+			try{
+				 mycommand.Connection = co;
+                myadapter.SelectCommand = mycommand;
+                mycommand.CommandText = "select * from Table_Barang";
+                DataSet ds= new DataSet();
+                if (myadapter.Fill(ds,"Table_Barang")>0){
+                    dataGridView1.DataSource = ds;
+                    dataGridView1.DataMember = "Table_Barang";
+                }
+                co.Close();
+            }
+            catch (Exception ex){
+                MessageBox.Show(ex.ToString());
+			}	
+		}
+		
+		void caribarang() {
+			
+			try{
+				 mycommand.Connection = co;
+                myadapter.SelectCommand = mycommand;
+                mycommand.CommandText = "select * from Table_Barang where kodebarang like '%"+textBox7.Text+"%' or namabarang like '%"+textBox7.Text+"%'";
+                DataSet ds= new DataSet();
+                if (myadapter.Fill(ds,"Table_Barang")>0){
+                    dataGridView1.DataSource = ds;
+                    dataGridView1.DataMember = "Table_Barang";
+                }
+                co.Close();
+            }
+            catch (Exception ex){
+                MessageBox.Show(ex.ToString());
+			}	
+		}
+		
+		void Button1Click(object sender, EventArgs e)
+		{ 
+			if(textBox1.Text.Trim() == "" || textBox2.Text.Trim() == "" || textBox3.Text.Trim() == "" ||textBox4.Text.Trim() == "" ||textBox5.Text.Trim() == "" ||comboBox1.Text.Trim() == "")
+			{
+				MessageBox.Show("Isi setiap kolom yang tersedia terlebih dahulu");
+			}
+			else
+			{
+				try{
+					mycommand.Connection = co;
+                	myadapter.SelectCommand = mycommand;
+                	mycommand.CommandText = "INSERT INTO TABLE_BARANG VALUES ('"+textBox1.Text+"', '"+textBox2.Text+"','"+textBox3.Text+"','"+textBox4.Text+"','"+textBox5.Text+"','"+comboBox1.Text+"')";
+                	DataSet ds= new DataSet();
+                	if (myadapter.Fill(ds,"Table_Barang")>0){
+                    	dataGridView1.DataSource = ds;
+                    	dataGridView1.DataMember = "Table_Barang";
+                }
+                MessageBox.Show("Data berhasil di input");
+                    	readdata();
+                    	Bersihkan();
+                co.Close();
+            }
+            catch (Exception ex){
+                MessageBox.Show("Data Gagal di input");
+			}
+				
+			}
+		}
+		
+		
+		
+		void DataGridView1CellClick(object sender, DataGridViewCellEventArgs e)
+		{
+		try 
+			{
+				DataGridViewRow row = this.dataGridView1.Rows [e.RowIndex];
+				textBox1.Text = row.Cells["kodeBarang"].Value.ToString();
+				textBox2.Text = row.Cells["NamaBarang"].Value.ToString();
+				textBox3.Text = row.Cells["HargaJual"].Value.ToString();
+				textBox4.Text = row.Cells["HargaBeli"].Value.ToString();
+				textBox5.Text = row.Cells["JumlahBarang"].Value.ToString();
+				comboBox1.Text = row.Cells["SatuanBarang"].Value.ToString();
+			}
+			catch (Exception ex) {
+				MessageBox.Show(ex.ToString());
+			}	
+		}
+		
+		
+		
+		
+		
+		void Button2Click(object sender, EventArgs e)
+		{
+			if(textBox1.Text.Trim() == "" || textBox2.Text.Trim() == "" || textBox3.Text.Trim() == "" ||textBox4.Text.Trim() == "" ||textBox5.Text.Trim() == "" ||comboBox1.Text.Trim() == "")
+			{
+				MessageBox.Show("Isi setiap kolom yang tersedia terlebih dahulu");
+			}
+			else
+			{
+				try{
+					mycommand.Connection = co;
+                	myadapter.SelectCommand = mycommand;
+                	mycommand.CommandText = "UPDATE Table_Barang SET KodeBarang='"+textBox1.Text+"',NamaBarang='"+textBox2.Text+"',HargaJual='"+textBox3.Text+"',HargaBeli='"+textBox4.Text+"',JumlahBarang='"+textBox5.Text+"',SatuanBarang='"+comboBox1.Text+"'where KodeBarang='"+textBox1.Text+"'";
+                	DataSet ds= new DataSet();
+                	if (myadapter.Fill(ds,"Table_Barang")>0){
+                    	dataGridView1.DataSource = ds;
+                    	dataGridView1.DataMember = "Table_Barang";
+                }
+                MessageBox.Show("Update Data berhasil!");
+                    	readdata();
+                    	Bersihkan();
+                co.Close();
+            }
+            catch (Exception ex){
+					MessageBox.Show(ex.ToString());
+			}
+				
+			}
+		}
+		
+		void Button3Click(object sender, EventArgs e)
+		{
+			if (MessageBox.Show("Yakin ingin menghapus "+textBox2.Text+"?", "Hapus Data", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+			{
+				try{
+					mycommand.Connection = co;
+                	myadapter.SelectCommand = mycommand;
+                	mycommand.CommandText = "DELETE FROM Table_Barang where KodeBarang='"+textBox1.Text+"'";
+                	DataSet ds= new DataSet();
+                	if (myadapter.Fill(ds,"Table_Barang")>0){
+                    	dataGridView1.DataSource = ds;
+                    	dataGridView1.DataMember = "Table_Barang";
+                }
+                MessageBox.Show("Hapus Data berhasil!");
+                    	readdata();
+                    	Bersihkan();
+                co.Close();
+            }
+				catch (Exception ex){
+					MessageBox.Show(ex.ToString());
+			}
+			}
+		}
+		
+		void TextBox7TextChanged(object sender, EventArgs e)
+		{
+			caribarang();
+		}
+		
+		void Button4Click(object sender, EventArgs e)
+		{
+			Bersihkan();
+		}
+	}
+}
